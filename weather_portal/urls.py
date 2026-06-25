19e46ed18af8cee83d40ccb8e150a6014f21d399
RanaMazharKhan <ranamazhar1450@gmail.com>
2026-06-25 23:17:29 +0500

    Add and route google-site-verification HTML file

--- weather_portal/urls.py
+++ weather_portal/urls.py
@@ -4,8 +4,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('google0574e06f59cbf9df.html', TemplateView.as_view(template_name='google0574e06f59cbf9df.html', content_type='text/html')),
     path('', include('weather.urls')),
     path('accounts/', include('accounts.urls')),
 ]
